710962bebbe20d34a85cc5b0e2128a0390d92bdd
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-02-03 15:56:47 -0800

    Create 4 to 1 mux.v

---
+++ Combinational Datapath Components: Adders and Subtractors/301 Review/4 to 1 mux/Design sources/4 to 1 mux.v
@@ -0,0 +1,17 @@
+module mux_4to1(
+    input a,
+    input b,
+    input c,
+    input d,
+    input [1:0]sel,
+    output out
+    );
+    
+    wire out1;
+    wire out2;
+    
+    mux_2to1 uut1(.a(a), .b(b), .c(sel[0]), .out(out1));
+    mux_2to1 uut2(.a(c), .b(d), .c(sel[0]), .out(out2));
+    mux_2to1 uut3(.a(out1), .b(out2), .c(sel[1]), .out(out));
+    
+endmodule
