c506494b155e96aaf8d2b0818002aff93fddd96d
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-02-03 16:28:27 -0800

    Create TwoInputORGate.v

---
+++ Boolean Algebra and Logic Gates/Design Sources/TwoInputORGate.v
@@ -0,0 +1,9 @@
+module twoinputorgate(
+    input in0,
+    input in1,
+    output o
+    );
+    
+    mux_4to1 uut1 (.a(1'b1), .b(1'b1), .c(1'b1), .d(1'b0), .sel({in1,in0}), .out(o));
+    
+endmodule
