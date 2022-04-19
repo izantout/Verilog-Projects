fc6ed5b09590ecb784888c439b9307d0d8a5085f
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-19 15:25:15 -0700

    Update Binary_Gray_tb.v

--- Dual Binary to Gray Convertor/Simulation Sources/Binary_Gray_tb.v
+++ Dual Binary to Gray Convertor/Simulation Sources/Binary_Gray_tb.v
@@ -1,1 +1,31 @@
-in
+`timescale 1ns / 1ps
+
+module Binary_Gray_tb (
+    );
+    
+    reg [3:0] num_tb; 
+    reg sel_tb;
+    wire [3:0] out_tb;
+    
+    Binary_Gray uut(.num(num_tb), .sel(sel_tb), .out(out_tb));
+    
+    initial begin
+        // Binary to Gray
+        sel_tb = 0;
+        num_tb = 4'b0110;
+        #20;
+
+        num_tb = 4'b0101;
+        #20;
+        
+        // Gray to Binary
+        sel_tb = 1;
+        num_tb = 4'b0101;
+        #20;
+	
+        num_tb = 4'b0110;
+        #20;
+
+	$finish;
+    end
+endmodule
